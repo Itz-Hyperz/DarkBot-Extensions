49b335d0101c908157dcfeb0be74ecda9ece682a
Itz-Hyperz <logan@hyperz.net>
2022-06-28 18:19:46 -0700

    Update template.js

--- template.js
+++ template.js
@@ -1,8 +1,8 @@
-module.exports = async function(client, con, app, darkbot) {
+module.exports = async function(client, con, app) {
 
     // client = Discord Bot Client
+    // client.darkbot = Custom event emitter (released in 1.3 update)
     // con = Database Connection
     // app = Express App
-    // darkbot = Custom event emitter (released in 1.3 update)
 
 };
