47be98fb64b7d90a5b0c2691cb757811b6a0fd32
Itz-Hyperz <logan@hyperz.net>
2022-06-28 23:43:00 -0700

    Delete template.js

--- template.js
+++
@@ -1,8 +0,0 @@
-module.exports = async function(client, con, app) {
-
-    // client = Discord Bot Client
-    // client.darkbot = Custom event emitter (released in 1.3 update)
-    // con = Database Connection
-    // app = Express App
-
-};
