5e780c048c459e5e72b26c78a48fd0bd4fe9f196
Itz-Hyperz <loganfgross@gmail.com>
2022-06-29 02:43:13 -0400

    Add files via upload

---
+++ [ TEMPLATE ]/main.js
@@ -0,0 +1,7 @@
+// https://docs.hyperz.net/c/products/darkbotbackend
+module.exports = async function(client, con, app) {
+    // client = Discord Bot Client
+    // client.darkbot = Custom event emitter (released in 1.3 update)
+    // con = Database Connection
+    // app = Express App
+};
